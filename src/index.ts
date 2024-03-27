ecb72fe33372539ef7f0c5ba4df1e74229532e06
adiletbekmuratov <dd01x3@gmail.com>
2024-03-27 20:42:01 +0500

    refresh random user

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { Telegraf } from "telegraf";
+import { InlineKeyboardMarkup } from "telegraf/types";
 import { MergeRequest } from "./types";
+
+function extractRandomAssignee(message: string) {
+  const regex = /Random assignee: @(\w+)/;
+  const match = message.match(regex);
+
+  if (match && match[1]) {
+    return match[1];
+  } else {
+    return null;
+  }
+}
+
+function getRandomUser(users: string[], excludeUser: string) {
+  let randomUser;
+
+  do {
+    randomUser = users[Math.floor(Math.random() * users.length)];
+  } while (randomUser === excludeUser);
+
+  return randomUser;
+}
 
 const users = [
   "teaking",
@@ -17,26 +39,61 @@ const users = [
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 const app = new Hono();
 
+bot.action("refresh_random", async (ctx) => {
+  try {
+    const assignee =
+      extractRandomAssignee((ctx.msg as { text: string })?.text) ?? "";
+    const random = getRandomUser(users, assignee);
+
+    await bot.telegram.editMessageText(
+      ctx.msg.chat.id,
+      ctx.msg.message_id,
+      undefined,
+      (ctx.msg as { text: string })?.text?.replaceAll(assignee, random),
+      {
+        parse_mode: "HTML",
+        reply_markup: (ctx.msg as { reply_markup: InlineKeyboardMarkup })
+          .reply_markup,
+      }
+    );
+  } catch (error) {
+    console.log(JSON.stringify(error, null, 2));
+    return;
+  }
+});
+
 app.post("/", async (c) => {
   const mrData: MergeRequest = await c.req.json();
   if (mrData.object_attributes.action !== "open") {
     return c.json(mrData);
   }
 
-  const random = Math.floor(Math.random() * users.length);
+  const random = getRandomUser(users, "");
   let msg = "";
   if (mrData.object_attributes.action === "open") {
-    msg = `**${mrData.user.username}** created new [Merge Request](${mrData.object_attributes.url}).\n\nTitle: ${mrData.object_attributes.title}\nDescription: ${mrData.object_attributes.description}\n\nRandom assignee: @${users[random]}`;
+    msg = `<b>${mrData.user.username}</b> created new <b>Merge Request</b>.\n\nTitle: ${mrData.object_attributes.title}\nDescription: ${mrData.object_attributes.description}\n\nRandom assignee: @${random}`;
     const channels = process.env.CHANNEL_ID!.split(";");
     channels.forEach(async (channel) => {
       await bot.telegram.sendMessage(channel, msg, {
-        parse_mode: "Markdown",
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Open Merge Request", url: mrData.object_attributes.url }],
+            [{ text: "Refresh Random User", callback_data: "refresh_random" }],
+          ],
+        },
       });
     });
   }
 
   return c.json(mrData);
 });
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
